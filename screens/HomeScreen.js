d65266ab8cbd5395c9fcbe02af409c15aab805a6
André Prol <andreprol1980@gmail.com>
2019-06-10 20:08:14 -0300

    Primeira parte do Instagram feita

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import {
+  Platform,
   StyleSheet,
+  Text,
   View,
+  Image,
+  FlatList,
+  TouchableOpacity
 } from 'react-native';
-
+import Lista from '../src/components/Lista';
 
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { 
+    
+      feed: [
+        {id: '1', nome: 'Lucas Silva', descricao: 'Mais um dia de muitos bugs :)', imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  likeada: false, likers: 0},
+        {id: '2', nome: 'Matheus', descricao: 'Isso sim é ser raiz!!!!!', imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', likeada: false, likers: 0},
+        {id: '3', nome: 'Jose Augusto', descricao: 'Bora trabalhar Haha', imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  likeada: false, likers: 3},
+        {id: '4', nome: 'Gustavo Henrique', descricao: 'Isso sim que é TI!', imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', likeada: false, likers: 1},
+        {id: '5', nome: 'Guilherme', descricao: 'Boa tarde galera do insta...', imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png', likeada: false, likers: 32}
+      ]
+
+     };
+  }
+
   static navigationOptions = {
     header: null,
   };
@@ -15,7 +36,11 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MonoText> HomeScreen </MonoText>
+
+        <FlatList keyExtractor={ item => item.id}
+                  data={this.state.feed} 
+                  renderItem={({item}) => <Lista data={item} />}/>
+
       </View>
     )
   }
@@ -25,8 +50,7 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    
   }
 
 });
