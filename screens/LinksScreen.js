ade24bd4e73dd3ff56f6159946e6ede19f28c995
André Prol <andreprol1980@gmail.com>
2019-06-12 20:21:30 -0300

    numeros aleatorios em andamento

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -2,31 +2,60 @@ import React from 'react';
 import {
   StyleSheet,
   View,
+  TouchableOpacity,
+  Text
 } from 'react-native';
- 
+
 
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lop:[1,2,3,4,5,6],
+    }
+    this.gerarnumero=this.gerarnumero.bind(this)
+  }
+
   static navigationOptions = {
     header: null,
   };
 
+  gerarnumero() {
+    let x = 6
+    
+    for (let i = 0; i > x; i++) {
+      this.lop[i] = (Math.floor(Math.random() * 10 + 1))
+    }
+
+    alert(this.lop)
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <MonoText> LinksScreen </MonoText>
+        <Text>Aperte o botão abaixo para gerar o número</Text>
+        <TouchableOpacity style={styles.buttonStyle} onPress={this.gerarnumero}>
+          <Text>Gerar</Text>
+        </TouchableOpacity>
       </View>
     )
   }
-   
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems:'center'
+    alignItems: 'center'
+  },
+  buttonStyle: {
+    marginTop: 15,
+    padding: 10,
+    backgroundColor: 'green',
+    borderRadius: 5
   }
-  
 });
